30e7ad3de8cab0a282646c597eb23c72edf825c7
Maria Eduarda N <116231205+MariaEduardaN@users.noreply.github.com>
2022-11-30 21:03:02 -0300

    JavaScript: cemáforo
    30/11/2022

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,18 @@
 function verde(){
-    document.getElementById('sinal').style.backgroundColor = 'green'
+    branco()
+    document.getElementById('verde').style.backgroundColor = 'green'
 }
 function amarelo(){
-    document.getElementById('sinal').style.backgroundColor = 'yellow'
+    branco()
+    document.getElementById('amarelo').style.backgroundColor = 'yellow'
 }
 function vermelho(){
-    document.getElementById('sinal').style.backgroundColor = 'red'
+    branco()
+    document.getElementById('vermelho').style.backgroundColor = 'red'
+}
+
+function branco(){
+    document.getElementById('vermelho').style.backgroundColor = 'white'
+    document.getElementById('verde').style.backgroundColor = 'white'
+    document.getElementById('amarelo').style.backgroundColor = 'white'
 }
